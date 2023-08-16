2a7c7bafac1706ea483af7613b5011ecd96b7c26
Andebrhan <anteforedu@gmail.com>
2023-08-15 23:07:49 -0400

    edited

--- discovery-service/src/main/java/com/cydeo/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/cydeo/DiscoveryServiceApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer//to enable the service working as eureka server
 public class DiscoveryServiceApplication {
-
     public static void main(String[] args) {
-
         SpringApplication.run(DiscoveryServiceApplication.class);
     }
 }
