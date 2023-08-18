81af2a0f558f55cfaed056f97705bbaa70d5a35d
Andebrhan <anteforedu@gmail.com>
2023-08-18 00:04:06 -0400

    modified due to multiple instances

--- app-client-management/src/main/java/com/cydeo/service/UserClientService.java
+++ app-client-management/src/main/java/com/cydeo/service/UserClientService.java
@@ -1,12 +1,12 @@
 package com.cydeo.service;
 
-import com.cydeo.dto.UserDTO;
 import com.cydeo.dto.UserResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "user-service",url = "http://localhost:9090")
+@FeignClient(value = "user-service")//if we have several instance not need to specify the url
+//@FeignClient(value = "user-service",url = "http://localhost:9090")
 public interface UserClientService {
 
     @GetMapping("/api/v1/user/{username}")
