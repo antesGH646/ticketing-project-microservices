07e647bbb48f047f2c2edcabc7f2324c14f9fe1b
Andebrhan <anteforedu@gmail.com>
2023-08-24 15:17:33 -0400

    dependency adding service to swagger

--- user-service/src/main/java/com/cydeo/UserServiceApplication.java
+++ user-service/src/main/java/com/cydeo/UserServiceApplication.java
@@ -1,5 +1,8 @@
 package com.cydeo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +11,13 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+//OpenAPI will add this service into the swagger
+@OpenAPIDefinition(servers = {@Server(url = "/user-service")},
+        info = @Info(title = "Ticketing App",
+        version = "1.0", description = "User Service API"))
 public class UserServiceApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(UserServiceApplication.class,args);
     }
 
